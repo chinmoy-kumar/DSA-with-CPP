7aefef393be84b814cc5c156a513a5d7733ad653
chinmoy.kumar <chinmoy.coder@gmail.com>
2024-12-17 23:50:34 +0600

    modify file

---
+++ singly_linked_list_operations/referance_of_a_pointer.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void func(int *&p){ // referance of a pointer
+    p = NULL;
+}
+
+int main(){
+    int x = 10;
+    int *p = &x;
+    func(p);
+    cout <<  "In Main: " << p << endl;
+
+    return 0;
+}
